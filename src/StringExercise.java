b6b85fa155d4f95519f3160c783273695083e284
Sam Lansford <sam.j.lansford@gmail.com>
2021-03-05 13:25:00 -0600

    doing java strings

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        System.out.println("We dont need educations\nWe don't need to thought control");
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println("In windows, the main drive is usually C:\\");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslashes \\\\\\!");
+    }
+}
