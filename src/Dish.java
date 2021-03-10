5db2983219c83449b427b8257ffc76e0d16843a0
Sam Lansford <sam.j.lansford@gmail.com>
2021-03-10 08:55:34 -0600

    making classes in java

--- src/Dish.java
+++ src/Dish.java
@@ -4,7 +4,7 @@ public class Dish {
     public String nameOfDish;
     public boolean wouldRecommend;
 
-    public void printSummary() {
-        System.out.printf("Your total is %d cents. For the dish %s, would you like to recommend this to others? %b", costInCents, nameOfDish, wouldRecommend);
-    }
+
+
+
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,10 +1,17 @@
+import java.util.Locale;
+
 public class DishTest {
     public static void main(String[] args) {
 
-    Dish d1 = new Dish();
-    d1.costInCents = 1000;
-    d1.nameOfDish = "Caesar salad";
-    d1.wouldRecommend = true;
-    d1.printSummary();
+        Dish d1 = new Dish();
+        d1.costInCents = 1400;
+        d1.nameOfDish = "Caesar salad";
+        d1.wouldRecommend = true;
+        DishTools.printSummary(d1);
+
+        DishTools.shoutDishName(d1);
+        DishTools.printShoutDishName(DishTools.shoutDishName(d1));
+        DishTools.analyzeDishCost(d1);
+        DishTools.flipRecommendation(d1);
     }
 }
