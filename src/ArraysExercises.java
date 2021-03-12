2040031e040445200638b7ac6c812472822c0f1c
Sam Lansford <sam.j.lansford@gmail.com>
2021-03-12 10:56:32 -0600

    doing arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,37 @@
+import java.util.Arrays;
+
+
 public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] peoples = new Person[3];
+        peoples[0] = new Person("Sam");
+        peoples[1] = new Person("John");
+        peoples[2] = new Person("Jeff");
+
+        for (Person people : peoples) {
+            System.out.println(people.getName());
+        }
+
+        peoples = addPerson(peoples, new Person("Bill"));
+
+        for (Person people : peoples) {
+            System.out.println(people.getName());
+        }
+
+
+    }
+
+    public static Person[] addPerson(Person[] people, Person person) {
+
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+
+        newPeople[newPeople.length - 1] = person;
+
+        return newPeople;
+    }
+
+
 }
