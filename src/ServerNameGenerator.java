38a47c1e5d6a7475acd981f51f6d2cee9648607f
Sam Lansford <sam.j.lansford@gmail.com>
2021-03-12 12:06:19 -0600

    making a random words genreator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+        String[] adjectives = {"Charming", "Cruel", "Fantastic", "Gentle", "Huge", "small", "big", "light", "dark", "sharp"};
+        String[] nouns = {"cat", "dog", "people", "Bob", "John", "hat", "bat", "bird", "fish"};
+
+        Random random = new Random();
+        int randomAdj = random.nextInt(adjectives.length);
+        int randomNoun = random.nextInt(nouns.length);
+        System.out.println(adjectives[randomAdj] + "-" + nouns[randomNoun]);
+
+    }
+
 }
