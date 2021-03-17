540201b1830de452f980a5b9502a6580c9dbee15
Sam Lansford <sam.j.lansford@gmail.com>
2021-03-17 13:35:07 -0500

    doing movies array

---
+++ src/grades/Student.java
@@ -0,0 +1,17 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+
+    private ArrayList<Integer> grade;
+
+    public Student(String name, ArrayList<Integer> grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+}
