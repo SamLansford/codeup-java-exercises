23e09251c15579a40c473182dec6f53686a6abe0
Sam Lansford <sam.j.lansford@gmail.com>
2021-08-11 23:45:03 -0500

    doing jumbled numbers

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -14,13 +14,77 @@ public class JavaIIIWarmUp {
 //        System.out.println("The mode of of all the number is: " + mode(myArray));
 //        System.out.println("The histogram: ");
 //        histogram(myArray);
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a sentence to convert to camel case");
-        String userInput = sc.nextLine();
-        sc.close();
-        System.out.println("You entered:" + userInput);
-        System.out.println("Output is: " + convertToCamelCase(userInput));
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a sentence to convert to camel case");
+//        String userInput = sc.nextLine();
+//        sc.close();
+//        System.out.println("You entered:" + userInput);
+//        System.out.println("Output is: " + convertToCamelCase(userInput));
+        String s = "osefkjself";
+        System.out.println(finddigits(s));
     }
+
+    static String finddigits(String s)
+    {
+
+        // Strings of digits 0-9
+        String[] num
+                = { "zero", "one", "two",   "three", "four",
+                "five", "six", "seven", "eight", "nine" };
+
+        // Initialize vector
+        int[] arr = new int[10];
+
+        // Initialize answer
+        String ans = "";
+
+        // Size of the string
+        int n = s.length();
+
+        // Traverse the string
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'z')
+                arr[0]++;
+            if (s.charAt(i) == 'w')
+                arr[2]++;
+            if (s.charAt(i) == 'g')
+                arr[8]++;
+            if (s.charAt(i) == 'x')
+                arr[6]++;
+            if (s.charAt(i) == 'v')
+                arr[5]++;
+            if (s.charAt(i) == 'o')
+                arr[1]++;
+            if (s.charAt(i) == 's')
+                arr[7]++;
+            if (s.charAt(i) == 'f')
+                arr[4]++;
+            if (s.charAt(i) == 'h')
+                arr[3]++;
+            if (s.charAt(i) == 'i')
+                arr[9]++;
+        }
+
+        // Update the elements of the vector
+        arr[7] -= arr[6];
+        arr[5] -= arr[7];
+        arr[4] -= arr[5];
+        arr[1] -= (arr[2] + arr[4] + arr[0]);
+        arr[3] -= arr[8];
+        arr[9] -= (arr[5] + arr[6] + arr[8]);
+
+        // Print the digits into their
+        // original format
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                ans += (char)(i + '0');
+            }
+        }
+
+        // Return answer
+        return ans;
+    }
+
 
     public static String convertToCamelCase(String s) {
         String words[] = s.split("\\s+");
