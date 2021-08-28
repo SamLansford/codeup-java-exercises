eae43621cf789637dd7e9478aba6f594ab34ff72
Sam Lansford <sam.j.lansford@gmail.com>
2021-08-28 14:26:10 -0500

    calculate average of n numbers

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -50,21 +50,26 @@ public class JavaIIIWarmUp {
 //        System.out.println("Remainder = " + remainder);
 //        System.out.println(fizzBuzz(24));
 
-        double  dis,amount,markedprice,s;
+//        double  dis,amount,markedprice,s;
+//        markedprice=1000;
+//        dis=25;
+//        System.out.println("markedprice= "+markedprice);
+//        System.out.println("discount rate="+dis);
+//        s=100-dis;
+//        amount= (s*markedprice)/100;
+//        System.out.println("amount after discount="+amount);
 
-        markedprice=1000;
-
-        dis=25;
-
-        System.out.println("markedprice= "+markedprice);
-
-        System.out.println("discount rate="+dis);
-
-        s=100-dis;
-
-        amount= (s*markedprice)/100;
-
-        System.out.println("amount after discount="+amount);
+        int n;double res=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter how many numbers to cal  avg");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("enter   "+n+"  numbers");
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+            res =res+a[i];
+        System.out.println("average="+res/n);
 
     }
 
