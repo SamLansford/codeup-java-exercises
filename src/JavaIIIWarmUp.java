8b1ffef1b3586bb5e2152d9db4d656e8bb8b088b
Sam Lansford <sam.j.lansford@gmail.com>
2021-08-29 17:05:57 -0500

    Calculate distance between 2 points

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -59,17 +59,23 @@ public class JavaIIIWarmUp {
 //        amount= (s*markedprice)/100;
 //        System.out.println("amount after discount="+amount);
 
-        int n;double res=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter how many numbers to cal  avg");
-        n=sc.nextInt();
-        int a[]=new int[n];
-        System.out.println("enter   "+n+"  numbers");
-        for(int i=0;i<n;i++)
-            a[i]=sc.nextInt();
-        for(int i=0;i<n;i++)
-            res =res+a[i];
-        System.out.println("average="+res/n);
+//        int n;double res=0;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter how many numbers to cal  avg");
+//        n=sc.nextInt();
+//        int a[]=new int[n];
+//        System.out.println("enter   "+n+"  numbers");
+//        for(int i=0;i<n;i++)
+//            a[i]=sc.nextInt();
+//        for(int i=0;i<n;i++)
+//            res =res+a[i];
+//        System.out.println("average="+res/n);
+
+        int x1,x2,y1,y2;
+        double dis;
+        x1=1;y1=1;x2=4;y2=4;
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
 
     }
 
