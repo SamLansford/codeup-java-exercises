24045d3f108687bd33ad914f6e52836127f87a22
Sam Lansford <sam.j.lansford@gmail.com>
2021-09-11 15:46:00 -0500

    leap year in static class

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -234,23 +234,45 @@ public class JavaIIIWarmUp {
 //        if (i == len)
 //            System.out.println(key + " doesn't exist in array.");
 
-        long a,y,c;
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter  any  calendar year :");
-        y=sc.nextLong();
-        if(y!=0)
-        {
-            a=(y%400==0)?(c=1):((y%100==0)?(c=0):((y%4==0)?(c=1):(c=0)));
-            if(a==1)
-                System.out.println(y+" is a leap year");
-            else
-                System.out.println(y+" is not a leap year");
-        }
-        else
-            System.out.println("year zero does not exist ");
+//        long a,y,c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter  any  calendar year :");
+//        y=sc.nextLong();
+//        if(y!=0)
+//        {
+//            a=(y%400==0)?(c=1):((y%100==0)?(c=0):((y%4==0)?(c=1):(c=0)));
+//            if(a==1)
+//                System.out.println(y+" is a leap year");
+//            else
+//                System.out.println(y+" is not a leap year");
+//        }
+//        else
+//            System.out.println("year zero does not exist ");
 
+        long year;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter  any calendar year :");
+        year=sc.nextLong();
+        leapOrNot(year);
     }
 
+
+    static void leapOrNot(long year)
+    {
+        if(year!=0)
+        {
+            if(year%400==0)
+                System.out.println(year+" is a leap year");
+            else  if(year%100==0)
+                System.out.println(year+" is not a leap year");
+            else if(year%4==0)
+                System.out.println(year+" is a leap year");
+            else
+                System.out.println(year+" is not a leap year");
+        }
+        else
+            System.out.println("Year zero does not exist ");
+    }
 
     static String hexdecimal(int q)
     {
