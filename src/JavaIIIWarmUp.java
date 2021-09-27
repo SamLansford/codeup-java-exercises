5aebf0aa8d10180a1946c599eac1bb4c1c147f70
Sam Lansford <sam.j.lansford@gmail.com>
2021-09-26 22:46:54 -0500

    Subtract Two Matrices

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -294,25 +294,74 @@ public class JavaIIIWarmUp {
 //
 //        }
 
-        int n=5,p=3,result=1;
+//        int n=5,p=3,result=1;
+//
+//        if(n>=0&&p==0)
+//        {
+//            result=1;
+//        }
+//        else if(n==0&&p>=1)
+//        {
+//            result=0;
+//        }
+//        else
+//        {
+//            for(int i=1;i<=p;i++)
+//            {
+//                result=result*n;
+//            }
+//        }
+//        System.out.println(n+"^"+p+"="+result);
 
-        if(n>=0&&p==0)
-        {
-            result=1;
-        }
-        else if(n==0&&p>=1)
-        {
-            result=0;
-        }
-        else
-        {
-            for(int i=1;i<=p;i++)
-            {
-                result=result*n;
-            }
-        }
-        System.out.println(n+"^"+p+"="+result);
 
+        int row, col,i,j;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows");
+        row = in.nextInt();
+
+        System.out.println("Enter the number  columns");
+        col  = in.nextInt();
+
+        int mat1[][] = new int[row][col];
+        int mat2[][] = new int[row][col];
+        int res[][] = new int[row][col];
+
+        System.out.println("Enter the elements of matrix1");
+
+        for (  i= 0 ; i < row ; i++ )
+        {
+
+            for ( j= 0 ; j < col ;j++ )
+                mat1[i][j] = in.nextInt();
+
+        }
+        System.out.println("Enter the elements of  matrix2");
+
+
+        for (  i= 0 ; i < row ; i++ )
+        {
+
+            for ( j= 0 ; j < col ;j++ )
+                mat2[i][j] = in.nextInt();
+
+
+        }
+
+
+        for (  i= 0 ; i < row ; i++ )
+            for ( j= 0 ; j < col ;j++ )
+                res[i][j] =  mat1[i][j] - mat2[i][j]  ;
+
+        System.out.println("subtract  of  two matrices:-");
+
+        for (  i= 0 ; i < row ; i++ )
+        {
+            for ( j= 0 ; j < col ;j++ )
+                System.out.print(res[i][j]+"\t");
+
+            System.out.println();
+        }
     }
 
 
