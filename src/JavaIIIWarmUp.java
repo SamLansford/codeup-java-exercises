ce598fd11828223beb25140cb8d045663c6f9e6d
Sam Lansford <sam.j.lansford@gmail.com>
2021-09-28 22:40:16 -0500

    LCM Of Two Numbers

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -364,34 +364,58 @@ public class JavaIIIWarmUp {
 //        }
 
 
-        long n1,n2;
+//        long n1,n2;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter  number 1");
+//        n1=sc.nextLong();
+//        System.out.println("enter  number 2");
+//        n2=sc.nextLong();
+//        if(n1>0 && n2>0)
+//        {
+//            long result=gcdCal(n1,n2);
+//            System.out.println("Gcd of two numbers = "+result);
+//        }
+//        else
+//            System.out.println("Please enter numbers greater than zero");
+//    }
+//    static long gcdCal(long a,long b)
+//    {
+//        while (b > 0)
+//        {
+//            long temp = b;
+//            b = a % b;
+//            a = temp;
+//        }
+//        return a;
+
+
+        long a,b,lcm;
         Scanner sc=new Scanner(System.in);
         System.out.println("enter  number 1");
-        n1=sc.nextLong();
+        a=sc.nextLong();
         System.out.println("enter  number 2");
-        n2=sc.nextLong();
-        if(n1>0 && n2>0)
-        {
-            long result=gcdCal(n1,n2);
-            System.out.println("Gcd of two numbers = "+result);
-        }
-        else
-            System.out.println("Please enter numbers greater than zero");
+        b=sc.nextLong();
+        lcm=lcmCalculation(a,b);
+        System.out.println("LCM of "+a+" and "+b+" is ="+lcm);
     }
-    static long gcdCal(long a,long b)
+
+
+    static long lcmCalculation(long n1,long n2)
     {
-        while (b > 0)
+        long temp,i=2,res;
+        if(n1>n2)
+            res=n1;
+        else
+            res=n2;
+        temp=res;
+        while(res%n1!=0 || res%n2!=0)
         {
-            long temp = b;
-            b = a % b;
-            a = temp;
+            res=temp*i;
+            i++;
         }
-        return a;
-
+        return res;
 
     }
-
-
 
     static void leapOrNot(long year)
     {
