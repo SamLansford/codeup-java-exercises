2de6ae82a468e17bb3cf113b116bebb14e1c232c
Sam Lansford <sam.j.lansford@gmail.com>
2021-09-29 22:37:03 -0500

    Calculate Distance Between Two Points

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -389,14 +389,42 @@ public class JavaIIIWarmUp {
 //        return a;
 
 
-        long a,b,lcm;
+//        long a,b,lcm;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter  number 1");
+//        a=sc.nextLong();
+//        System.out.println("enter  number 2");
+//        b=sc.nextLong();
+//        lcm=lcmCalculation(a,b);
+//        System.out.println("LCM of "+a+" and "+b+" is ="+lcm);
+
+        int x1,x2,y1,y2;
+
+        double dis;
+
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter  number 1");
-        a=sc.nextLong();
-        System.out.println("enter  number 2");
-        b=sc.nextLong();
-        lcm=lcmCalculation(a,b);
-        System.out.println("LCM of "+a+" and "+b+" is ="+lcm);
+
+        System.out.println("enter x1 point");
+
+        x1=sc.nextInt();
+
+        System.out.println("enter y1 point");
+
+        y1=sc.nextInt();
+
+        System.out.println("enter x2point");
+
+        x2=sc.nextInt();
+
+        System.out.println("enter y2 point");
+
+        y2=sc.nextInt();
+
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+
+        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+
+
     }
 
 
