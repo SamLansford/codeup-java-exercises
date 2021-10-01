735369eed857ef4ae873c84dfe922429d4f8af6b
Sam Lansford <sam.j.lansford@gmail.com>
2021-09-30 22:52:04 -0500

    Sum Of digits Of A Number

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -398,34 +398,50 @@ public class JavaIIIWarmUp {
 //        lcm=lcmCalculation(a,b);
 //        System.out.println("LCM of "+a+" and "+b+" is ="+lcm);
 
-        int x1,x2,y1,y2;
+//        int x1,x2,y1,y2;
+//
+//        double dis;
+//
+//        Scanner sc=new Scanner(System.in);
+//
+//        System.out.println("enter x1 point");
+//
+//        x1=sc.nextInt();
+//
+//        System.out.println("enter y1 point");
+//
+//        y1=sc.nextInt();
+//
+//        System.out.println("enter x2point");
+//
+//        x2=sc.nextInt();
+//
+//        System.out.println("enter y2 point");
+//
+//        y2=sc.nextInt();
+//
+//        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+//
+//        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+//
 
-        double dis;
 
+        long n,sum;
         Scanner sc=new Scanner(System.in);
-
-        System.out.println("enter x1 point");
-
-        x1=sc.nextInt();
-
-        System.out.println("enter y1 point");
-
-        y1=sc.nextInt();
-
-        System.out.println("enter x2point");
-
-        x2=sc.nextInt();
-
-        System.out.println("enter y2 point");
-
-        y2=sc.nextInt();
-
-        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
-        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+        System.out.println("Enter a number ");
+        n=sc.nextLong();
+        for(sum=0 ;n!=0 ;n/=10)
+        {
+            sum+=n%10;
+        }
+        System.out.println("Sum of digits of a number is "+sum);
+    }
 
 
     }
+
+
+
 
 
     static long lcmCalculation(long n1,long n2)
