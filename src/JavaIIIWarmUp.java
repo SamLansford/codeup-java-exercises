f9f44dbb4128ca6d21b0aa8a1e1b819ee3869943
Sam Lansford <sam.j.lansford@gmail.com>
2021-10-02 22:52:13 -0500

    Perfect Number

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -426,14 +426,34 @@ public class JavaIIIWarmUp {
 //
 
 
-        long n,s;
+//        long n,s;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter a number ");
+//        n=sc.nextLong();
+//        s=sum(n);
+//        System.out.println("Sum of digits of a number is "+s);
+
+
+        long n,sum=0;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number ");
+        System.out.println("Enter a number");
         n=sc.nextLong();
-        s=sum(n);
-        System.out.println("Sum of digits of a number is "+s);
-
-
+        int i=1;
+        while(i<=n/2)
+        {
+            if(n%i==0)
+            {
+                sum+=i;
+            }
+            i++;
+        }
+        if(sum==n)
+        {
+            System.out.println(n+" is a perfect number");
+        }
+        else
+            System.out.println(n+" is not a  perfect number");
+    }
 
     }
 
