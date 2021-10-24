be1dc74fbfa06a84aed0771bb4eb8791836b4602
Sam Lansford <sam.j.lansford@gmail.com>
2021-10-23 22:33:10 -0500

    find the middle number in user input

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -556,6 +556,13 @@ public class JavaIIIWarmUp {
 
         }
 
+        if ((num1 != num2) && (num1 != num3)) {
+            System.out.println(num1);
+        } else if ((num2 != num1) && (num2!= num3)) {
+            System.out.println(num2);
+        } else if ((num3 !=  num1) && (num3 <=  num2)) {
+            System.out.println(num3);
+
     }
 
     static int armstrongOrNot(int num)
