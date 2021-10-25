109c10da52128ea8d97c022f073a9114a700f8d8
Sam Lansford <sam.j.lansford@gmail.com>
2021-10-24 21:16:18 -0500

    find the maximum number

--- src/JavaIIIWarmUp.java
+++ src/JavaIIIWarmUp.java
@@ -547,21 +547,31 @@ public class JavaIIIWarmUp {
 
         System.out.println("The inputs in sorted order are:");
 
-        if ((num1 < num2) && (num1 < num3)) {
-            System.out.println(num1);
-        } else if ((num2 < num1) && (num2 < num3)) {
-            System.out.println(num2);
-        } else if ((num3 < num1) && (num3 < num2)) {
-            System.out.println(num3);
+//        if ((num1 < num2) && (num1 < num3)) {
+//            System.out.println(num1);
+//        } else if ((num2 < num1) && (num2 < num3)) {
+//            System.out.println(num2);
+//        } else if ((num3 < num1) && (num3 < num2)) {
+//            System.out.println(num3);
+//
+//        }
 
+//        if ((num1 != num2) && (num1 != num3)) {
+//            System.out.println(num1);
+//        } else if ((num2 != num1) && (num2!= num3)) {
+//            System.out.println(num2);
+//        } else if ((num3 !=  num1) && (num3 <=  num2)) {
+//            System.out.println(num3);
+//        }
+
+
+        if ((num1 > num2) && (num1 > num3)) {
+            System.out.println(num1);
+        } else if ((num2 > num1) && (num2 > num3)) {
+            System.out.println(num2);
+        } else if ((num3 > num1) && (num3 > num2)) {
+            System.out.println(num3);
         }
-
-        if ((num1 != num2) && (num1 != num3)) {
-            System.out.println(num1);
-        } else if ((num2 != num1) && (num2!= num3)) {
-            System.out.println(num2);
-        } else if ((num3 !=  num1) && (num3 <=  num2)) {
-            System.out.println(num3);
 
     }
 
